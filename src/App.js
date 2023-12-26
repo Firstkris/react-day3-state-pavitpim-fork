28d12d4a7494894955159135b86f0963566495e0
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-26 16:17:22 +0700

    finish:lab-2 select tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-// จงสร้าง tag <input>
-// เมื่อผู้ใช้พิมพ์ค่าลงใน <input> ให้ console.log(ค่าที่ผู้ใช้พิมพ์)
+// Lab2
+// จงสร้าง tag <select> ซึ่งมีข้อมูลชื่อประเทศในรายการให้เลือก (ให้ลองใส่มา 5 ประเทศ)
+// เมื่อผู้ใช้เลือกประเทศ ให้ console.log ค่าประเทศที่ถูกเลือก
 
 function App() {
-  // handle fn
-  const handleChange = (event) => console.log(event.target.value);
+  // write your code
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
 
-  // Render UI
-  return <input onChange={handleChange} />;
+  return (
+    <select onChange={handleChange}>
+      <option disabled>select country</option>
+      <option value='TH'>thailand</option>
+      <option value='SG'>singapore</option>
+      <option value='LAO'>laos</option>
+      <option value='MM'>myanmar</option>
+      <option value='CN'>china</option>
+    </select>
+  );
 }
 
 export default App;
