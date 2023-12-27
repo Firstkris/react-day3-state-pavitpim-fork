ff9dfb6927a4832b0d63cf2966b2419030f83527
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-27 11:05:55 +0700

    finish:lab2-3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
 import React from 'react';
 
-function App() {
-  const [isShow,setIsShow] = React.useState(true)
+// Lab-2
+// จงสร้าง Button มีข้อความว่า “Click to hide text”
+// เมื่อผู้ใช้ click button ให้ซ่อน Text
 
-  // 1. สร้าง UI คือปุ่ม ✅
-  // 2. คลิกได้ (ผูกฟังก์ชัน) => Web รู้ว่ามีการ click ✅
-  //      - สร้างฟังก์ชัน
-  //      - เอาฟังก์ชันไปผูกกับ UI ผ่าน attribute Event ต่างๆ
-  // 3. คลิกแล้วซ่อนปุ่ม (click แล้วเปลี่ยนเป็น state false)
-  //      - condition render ✅
-  //      - state (state-1 แสดงปุ่ม , state-2 ลบปุ่ม)
-  //           - how to initialize state
-  //           - how to update state
+/* 
+TASK 
+1. UI  Button, Text
+2. ทำให้ปุ่มคลิก (ผูก event)
+3a. ซ่อน TEXT : Condition Render
+   {true && <p>text</p>} แสดง text
+   {false && <p>text</p>} ซ่อน text
+3b. Click แล้วซ่อน (toggle boolean true => false)
+    toggle state
+*/
+
+// function App() {
+//   const [isShow, setIsShow] = React.useState(true);
+
+//   // Event Handler function
+//   const handleClick = () => {
+//     console.log('click');
+//     setIsShow(false);
+//   };
+//   return (
+//     <main className='app'>
+//       <button onClick={handleClick}>click to hide text</button>
+//       {isShow && <p>text</p>}
+//     </main>
+//   );
+// }
+
+// Lab3
+/*
+- จงสร้าง Button มีข้อความว่า “Hide”
+
+- เมื่อผู้ใช้ click button 
+  -  ให้ซ่อนข้อความเดิม "Hide"
+  -  และเปลี่ยนข้อความบน Button เป็น “Show” 
+  -  และซ่อน text
+  
+- เมื่อ click button อีกครั้ง
+    - ให้ซ่อนข้อความเดิม "Show"
+    -ให้แสดงข้อความและเปลี่ยนข้อความบน Button เป็น Hide
+    - แสดง text
+
+TASK
+1. UI -> Button, Text
+2. ทำให้คลิกได้
+3. สลับการแสดงผลในปุ่ม Hide <-> Show
+4. สลับการแสดงผล text (ไม่แสดงผล <-> แสดงผล)
+5. คลิกแล้วสลับการแสดง
+*/
+function App() {
+  // let isShow = true;
+  const [isShow, setIsShow] = React.useState(true);
+
+  // Handler Fn
   const handleClick = () => {
     console.log('click');
-    setIsShow(false)
+    // isShow = false;
+    // if (isShow === true) setIsShow(false);
+    // else if (isShow === false) setIsShow(true);
+
+    // setIsShow(!isShow);
+    setIsShow((cur) => !cur);
   };
   return (
     <main className='app'>
-     {isShow 
-      ? <button onClick={handleClick}>Click to hide me</button>
-      : null}
-      {/* false => null */}
-      {/* true => <button> */}
+      <button onClick={handleClick}>{isShow ? 'Hide' : 'Show'}</button>
+      {isShow ? <p>Text</p> : null}
     </main>
   );
 }
