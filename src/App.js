7f0e52dca8aa917413f16ecb05485b272f02dfd3
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-27 11:48:34 +0700

    form-boilerplate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App() {
-  return <div>Hello React!!!</div>;
+  // Handler Function
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submit');
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='form'>
+        {/* UserName */}
+        <div className='form-input'>
+          <label>username</label>
+          <input />
+        </div>
+
+        <button type='submit'>Register</button>
+        {/* <button type='button'>inside</button> */}
+      </form>
+    </>
+  );
 }
 
 export default App;
