b410ea598d256892fee69dd8a0d47777e713c787
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-27 10:44:42 +0700

    finish:lab-1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 function App() {
-  const [show, setShow] = React.useState(true);
+  const [isShow,setIsShow] = React.useState(true)
 
-  // handle FN
-  const toggleShow = () => {
-    setShow(!show);
+  // 1. สร้าง UI คือปุ่ม ✅
+  // 2. คลิกได้ (ผูกฟังก์ชัน) => Web รู้ว่ามีการ click ✅
+  //      - สร้างฟังก์ชัน
+  //      - เอาฟังก์ชันไปผูกกับ UI ผ่าน attribute Event ต่างๆ
+  // 3. คลิกแล้วซ่อนปุ่ม (click แล้วเปลี่ยนเป็น state false)
+  //      - condition render ✅
+  //      - state (state-1 แสดงปุ่ม , state-2 ลบปุ่ม)
+  //           - how to initialize state
+  //           - how to update state
+  const handleClick = () => {
+    console.log('click');
+    setIsShow(false)
   };
-
   return (
     <main className='app'>
-      {/* {show ? <h1>Show</h1> : null} */}
-      {show && <h1>Show</h1>}
-      <button onClick={toggleShow}>click</button>
+     {isShow 
+      ? <button onClick={handleClick}>Click to hide me</button>
+      : null}
+      {/* false => null */}
+      {/* true => <button> */}
     </main>
   );
 }
