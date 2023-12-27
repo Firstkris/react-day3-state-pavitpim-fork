a181202bb7e72ef3e41c7134981dcee0cd7f3d79
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-27 12:09:47 +0700

    controlled input tag with react state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
+import React from 'react';
+// Uncontrolled
+// Controlled
+
 function App() {
-  // Handler Function
+  // State Controlled
+  const [username, setUserName] = React.useState('');
+
+  // Handler Function <form>
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('submit');
   };
 
+  // Handler Function <input>
+  const handleUserNameChange = (event) => {
+    console.log('user typing..', event.target.value);
+    setUserName(event.target.value);
+  };
   return (
     <>
       <form onSubmit={handleSubmit} className='form'>
         {/* UserName */}
         <div className='form-input'>
           <label>username</label>
-          <input />
+          <input
+            type='text'
+            placeholder='username or account'
+            value={username} // value ที่แสดงผลขึ้นกับกับ React state
+            onChange={handleUserNameChange}
+          />
         </div>
 
         <button type='submit'>Register</button>
-        {/* <button type='button'>inside</button> */}
       </form>
     </>
   );
