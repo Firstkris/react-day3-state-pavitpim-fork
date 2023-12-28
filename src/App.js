4f19e797586a6c51c41eece91b2f580ef97b0bd0
pavitpim40_mbp <pavitpim@gmail.com>
2023-12-28 09:48:47 +0700

    setup state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from 'react';
+
+// React State ใช้งานนอก FunctionComponent ไม่ได้ ❌
+
 function App() {
-  return <div>Hello React!!!</div>;
+  // React State ใช้งานภายใน FunctionComponentn เท่่านั้น ✅
+  const [todos, setTodos] = React.useState(['HW', 'Hangout', 'HW']);
+
+  const todoRender = todos.map((todo, index) => <li key={index}>{todo}</li>);
+
+  return (
+    <div>
+      <h1>My Todo</h1>
+      <ul>{todoRender}</ul>
+    </div>
+  );
 }
 
 export default App;
+
+// ทวนเรื่อง Map
+// input : ["Hw","Hangout"]
+// output : [<li>Hw</li>, <li>Hangout</li>]
+// instruction : input -> output : todo -> <li>todo</li>
+// map : input.map(instruction) ==> สร้าง output
